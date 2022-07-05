f77c11f83c853d5eb74b0638c92fccd2bcdfb9a3
Simone Sada <si.sada93@gmail.com>
2022-07-05 14:54:03 +0200

    Aggiungere i file di progetto.

---
+++ Address.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Address
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Zip { get; set; }
+
+        public Address(string name, string surname, string street, string city, string province, string zip)
+        {
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            Zip = zip;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine("\n* ADDRESS: *");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Street: {Street}");
+            Console.WriteLine($"City: {City}");
+            Console.WriteLine($"Province: {Province}");
+            Console.WriteLine($"Zip: {Zip}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Address> addresses = new List<Address>();
+
+            string path = "C:\\Users\\Simone\\source\\repos\\csharp-lista-indirizzi\\addresses.csv";
+
+            if (File.Exists(path))
+            {
+                // File creation
+                StreamReader file = File.OpenText(path);
+
+                uint lineCount = 0;
+
+                // File read
+                while (!file.EndOfStream)
+                {
+                    lineCount++;
+
+                    try
+                    {
+                        string line = file.ReadLine();
+                        string[] values = line.Split(',');
+
+                        // Checks
+                        if (values.Length > 6)
+                        {
+                            throw new Exception($"Too many values at line {lineCount}");
+                        }
+
+                        foreach(string value in values)
+                        {
+                            if(value == "")
+                            {
+                                throw new Exception($"Missing value at line {lineCount}");
+                            }
+                        }
+
+                        // Creation of Addres obj and save
+                        Address address = new Address(values[0], values[1], values[2], values[3], values[4], values[5]);
+                        addresses.Add(address);
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                // Close file
+                file.Close();
+            }
+            else
+            {
+                Console.WriteLine("File doesn't exist!");
+            }
+
+
+            // Stampa
+            foreach(Address address in addresses)
+            {
+                if(address != addresses[0])
+                {
+                    address.ToString();
+                }
+                
+            }
+        }
+    }
+}
+
+//
